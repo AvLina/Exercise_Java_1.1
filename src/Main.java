50dd4eefab3b499887511951f1f0f88d06bfd9f9
Lina Avramenko <avlina3@gmail.com>
2021-10-27 22:56:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 2_000_000_000;
+        int y = 500_000_000;
+        int total = x + y;
+        System.out.println(total);
+    }
+}
+
